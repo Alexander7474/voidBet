78414e2e18858e975965d431a591129fb555dd02
Alexander7474 <alexandre.lanternier@outlook.fr>
2025-03-03 22:41:30 +0100

    ajout de la db

---
+++ base.sql
@@ -0,0 +1,83 @@
+-- Table tournois
+CREATE TABLE tournois (
+    id_tournoi SERIAL PRIMARY KEY,
+    nom_tournoi VARCHAR(100) NOT NULL,
+    date_debut DATE NOT NULL,
+    cash_prize NUMERIC(10, 2) NOT NULL
+);
+
+-- Table coachs
+CREATE TABLE coachs (
+    id_coach SERIAL PRIMARY KEY,
+    nom_coach VARCHAR(50) NOT NULL
+);
+
+-- Table equipes
+CREATE TABLE equipes (
+    id_equipe SERIAL PRIMARY KEY,
+    nom_equipe VARCHAR(50) NOT NULL,
+    id_coach INT,
+    FOREIGN KEY (id_coach) REFERENCES coachs(id_coach)
+);
+
+-- Table joueurs
+CREATE TABLE joueurs (
+    id_joueur SERIAL PRIMARY KEY,
+    pseudo VARCHAR(50) NOT NULL,
+    nationalite VARCHAR(30)
+);
+
+-- Table composition d'equipe (Multivalué)
+CREATE TABLE composition_Equipe (
+    id_equipe INT NOT NULL,
+    id_joueur INT NOT NULL,
+    PRIMARY KEY (id_equipe, id_joueur),
+    FOREIGN KEY (id_equipe) REFERENCES equipes(id_equipe),
+    FOREIGN KEY (id_joueur) REFERENCES joueurs(id_joueur)
+);
+
+-- Table mtchs
+CREATE TABLE matchs (
+    id_match SERIAL PRIMARY KEY,
+    date_match DATE NOT NULL,
+    heure_match TIME NOT NULL,
+    id_equipe1 INT NOT NULL,
+    id_equipe2 INT NOT NULL,
+    id_tournoi INT NOT NULL,
+    resultat VARCHAR(20),
+    FOREIGN KEY (id_equipe1) REFERENCES equipes(id_equipe),
+    FOREIGN KEY (id_equipe2) REFERENCES equipes(id_equipe),
+    FOREIGN KEY (id_tournoi) REFERENCES tournois(id_tournoi),
+    CHECK (id_equipe1 <> id_equipe2)
+);
+
+CREATE TABLE bet_on_score (
+  id_paris SERIAL PRIMARY KEY,
+  id_match INT NOT NULL,
+  score1 INT NOT NULL,
+  score2 INT NOT NULL,
+  date_paris DATE NOT NULL,
+  bet_result BOOLEAN,
+  FOREIGN KEY (id_match) REFERENCES matchs(id_match)
+);
+
+CREATE TABLE bet_on_result (
+  id_paris SERIAL PRIMARY KEY,
+  id_match INT NOT NULL,
+  result VARCHAR(10) NOT NULL,
+  id_equipe INT NOT NULL,
+  date_paris DATE NOT NULL,
+  bet_result BOOLEAN,
+  FOREIGN KEY (id_equipe) REFERENCES equipes(id_equipe),
+  FOREIGN KEY (id_match) REFERENCES matchs(id_match)
+);
+
+--Table users
+CREATE TABLE users (
+  id_user SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(200) NOT NULL,
+  email VARCHAR(200) NOT NULL,
+  void_coin DOUBLE PRECISION,
+  date_creation DATE
+);
