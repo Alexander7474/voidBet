783d761577f8e29efa61fe5c78f4bc954b1e0321
Alexander7474 <alexandre.lanternier@outlook.fr>
2025-03-04 08:41:15 +0100

    modification footer.php

--- templates/front/footer.php
+++ templates/front/footer.php
@@ -2,7 +2,7 @@
 <div class="container">
   <footer class="py-5">
     <div class="row">
-      <div class="col-8">
+      <div class="col-6">
         <h5>Naviguer</h5>
         <ul class="nav flex-column">
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
@@ -13,14 +13,15 @@
         </ul>
       </div>
 
-      <div class="col-4">
+      <div class="col-6">
         <form>
-          <h5>Subscribe to our newsletter</h5>
-          <p>Monthly digest of what's new and exciting from us.</p>
+          <h5>Contact us</h5>
           <div class="d-flex flex-column flex-sm-row w-100 gap-2">
             <label for="newsletter1" class="visually-hidden">Email address</label>
             <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-            <button class="btn btn-primary" type="button">Subscribe</button>
+            <label for="newsletter1" class="visually-hidden">Nom</label>
+zsh:1: command not found: :wqa
+            <button class="btn btn-primary" type="button">Envoyer</button>
           </div>
         </form>
       </div>
